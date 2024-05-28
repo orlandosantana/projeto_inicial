7fe44a06574102280b80950b4643fd6c41daac03
Orlando Santana <santanaaorlando@gmail.com>
2024-05-27 21:48:14 -0400

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 30
+nome = "Orlando"
+
+# Print
+
+print("nome: ", nome, "\n  idade: ", idade)
+
+# Soma
+new_age = idade + 1
+print(new_age)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
